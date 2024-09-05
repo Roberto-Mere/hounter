3491ca50faab1d742da4bcc2e6b637bda8414b3f
rmere <rmere@griddynamics.com>
2024-09-05 12:45:44 -0600

    Add data for dynamic generated features

---
+++ src/js/data.js
@@ -0,0 +1,23 @@
+export const data = {
+  heroFeatures: [
+    {
+      title: '1K+ People',
+      label: 'Successfully Getting Home',
+      images: [
+        'src/assets/img/avatar-ashley.png',
+        'src/assets/img/avatar-json.png',
+        'src/assets/img/avatar-jenny.png',
+      ],
+    },
+    {
+      title: '56 Houses',
+      label: 'Sold Monthly',
+      images: ['src/assets/img/articles-4.png'],
+    },
+    {
+      title: '4K+ People',
+      label: 'Looking for New Homes',
+      images: ['src/assets/img/avatar-ashley.png'],
+    },
+  ],
+};
