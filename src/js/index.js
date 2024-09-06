19357898076db2db56c001b4948d67918340b6b6
rmere <rmere@griddynamics.com>
2024-09-06 09:28:08 -0600

    Add initialiser function for reviews

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,10 @@
-import { createHeroFeatures, createHouseFeatures } from './dom';
+import { createHeroFeatures, createHouseFeatures, createReviews } from './dom';
 import { data } from './data';
 
 function init() {
   createHeroFeatures(data.heroFeatures);
   createHouseFeatures(data.houseFeatures);
+  createReviews(data.houseReviews);
 }
 
 init();
