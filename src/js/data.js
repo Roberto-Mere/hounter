b3ca8421feed8e27514f88c6174d4a9978e8a80c
rmere <rmere@griddynamics.com>
2024-09-06 09:10:12 -0600

    Add review data

--- src/js/data.js
+++ src/js/data.js
@@ -66,4 +66,42 @@ export const data = {
       },
     },
   ],
+  houseReviews: [
+    {
+      image: 'src/assets/img/reviews-1.png',
+      title: 'My house sold out fast!',
+      description:
+        'I was finally able to sell my house quickly through Hounter by immediately setting me up with people who wanted my house. I also do not get a discount from the sale of my house. Awesome!',
+      partner: {
+        image: 'src/assets/img/avatar-dianne.png',
+        name: 'Dianne Russell',
+        label: 'Head Director',
+      },
+      rating: 4.3,
+    },
+    {
+      image: 'src/assets/img/reviews-2.png',
+      title: 'Best! I got the house I wanted through Hounter',
+      description:
+        'Through this website I can get a house with the type and specifications I want very easily, without a complicated process to be able to find information on the house we want.',
+      partner: {
+        image: 'src/assets/img/avatar-jenny.png',
+        name: 'Jenny Wilson',
+        label: 'Manager Director',
+      },
+      rating: 4.6,
+    },
+    {
+      image: 'src/assets/img/reviews-3.png',
+      title: 'Through the Hounter, I can get a house for my self',
+      description:
+        'By looking for information about what kind of house we want, we managed to get the house we wanted very quickly, and directly connected with the seller to be able to ask about the details, very helpful!',
+      partner: {
+        image: 'src/assets/img/avatar-ronald.png',
+        name: 'Ronald Richards',
+        label: 'Head of Marketing',
+      },
+      rating: 4.6,
+    },
+  ],
 };
