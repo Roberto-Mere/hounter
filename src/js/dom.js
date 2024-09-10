adbd2f527e03284ec2b173c04f3d21846b0a7a48
rmere <rmere@griddynamics.com>
2024-09-10 16:02:45 -0600

    Add filter buttons event listener

--- src/js/dom.js
+++ src/js/dom.js
@@ -1,3 +1,23 @@
+export function addHandlerFilters(handler) {
+  const filtersContainer = document.querySelector('.features__filters');
+
+  filtersContainer.addEventListener('click', (e) => {
+    const filter = e.target.closest('.btn--filter');
+
+    if (!filter) return;
+
+    const filters = document.querySelectorAll('.btn--filter');
+
+    filters.forEach((filt) => {
+      filt.classList.remove('btn--filter-selected');
+    });
+
+    filter.classList.add('btn--filter-selected');
+
+    handler(filter.dataset.filter);
+  });
+}
+
 export function addHandlerMoreArticles(handler) {
   const moreArticles = document.querySelector('.articles__btn');
 
